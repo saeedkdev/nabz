46b6c2414ad182759faf1335051958bf0f8c27c8
Saeed Karimi <saeedsektor@gmail.com>
2023-06-10 10:39:50 -0700

    added ability to view songs

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,7 +2,15 @@ import Image from 'next/image'
 import Header  from '@/components/Header'
 import ListItem from '@/components/ListItem'
 
-export default function Home() {
+import getSongs from '@/actions/getSongs'
+import PageContent from './components/PageContent';
+
+export const revalidate = 0;
+
+export default async function Home() {
+
+    const songs = await getSongs()
+
     return (
         <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
             <Header>
@@ -25,7 +33,7 @@ export default function Home() {
                     <h1 className="text-white text-2xl font-semibold">Latest Releases</h1>
                 </div>
                 <div>
-                    List of songs
+                    <PageContent songs={songs} />
                 </div>
             </div>
                 
